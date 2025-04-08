b35fd6b2ca932282aca19bdd90924d6776d3f033
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-08 12:36:25 +0530

    string conversion in JS

--- core-js-concepts.js
+++ core-js-concepts.js
@@ -7,3 +7,9 @@ let a = '100';
 console.log(Number(100));
 console.log(Boolean(-1)); //will give false only for 0 and true for every number including negatives
 console.log(String(100)); 
+
+
+//comparisions
+console.log(true + "shubham"); //this will convert this into string
+console.log("shubham" + false);
+//anything with string in addition will conver that to string
