118e8dd99263f9cd745050b48953bfffe1dddf24
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-08 12:34:10 +0530

    type conversions in JS

--- core-js-concepts.js
+++ core-js-concepts.js
@@ -1,3 +1,9 @@
 //nullish coalescing operator (??)
 //It will return the right operand if the left one is null or undef
 console.log(undefined ?? "Shubham");
+
+//Type conversions
+let a = '100';
+console.log(Number(100));
+console.log(Boolean(-1)); //will give false only for 0 and true for every number including negatives
+console.log(String(100)); 
