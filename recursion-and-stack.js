7740286b599ee6f04dced938377f73e54a9c52bc
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-09 15:39:27 +0530

    stack in JS

--- recursion-and-stack.js
+++ recursion-and-stack.js
@@ -1,3 +1,4 @@
+//recursion
 function fact(n) {
   if(n < 0) return 0;
   if(n === 0 || n === 1) return 1;
@@ -5,3 +6,29 @@ function fact(n) {
 }
 
 console.log(fact(11));
+
+//stack
+const st = [];
+
+function pushInStack(num) {
+  st.unshift(num);
+}
+
+function popFromStack() {
+  st.shift();
+}
+
+function printStack() {
+  for(let i of st) console.log(i); 
+}
+
+pushInStack(5);
+pushInStack(6);
+pushInStack(7);
+pushInStack(8);
+
+printStack();
+
+popFromStack();
+console.log("after popping the top element stack is: ")
+printStack();
