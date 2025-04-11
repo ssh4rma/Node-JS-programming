f8ec376c8563793815b3fd432f2c2984759e3395
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-11 16:53:07 +0530

    Promise scheduling

--- scheduling.js
+++ scheduling.js
@@ -7,3 +7,10 @@ const id = setInterval(() => {
   console.log(`Tick ${cnt++}`);
   if(cnt === 3) clearInterval(id);
 }, 1000);
+
+//modern scheduling with async and await
+async function delay(ms) {
+  await new Promise(resolve => setTimeout(resolve, ms)); 
+  console.log("Hello");
+}
+delay(1000);
