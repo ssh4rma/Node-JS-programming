949d3b59f54f06aaac406498f04442e200baab88
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-11 16:05:41 +0530

    setInterval scheduling method

--- scheduling.js
+++ scheduling.js
@@ -1,3 +1,9 @@
 setTimeout(() => {
   console.log("Timeout for 5000ms")
 }, 5000);
+
+let cnt = 0;
+const id = setInterval(() => {
+  console.log(`Tick ${cnt++}`);
+  if(cnt === 3) clearInterval(id);
+}, 1000);
