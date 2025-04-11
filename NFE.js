c524f3d74799eea35b48c853aeb932501e6028f7
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-11 15:33:54 +0530

    NFE type2

--- NFE.js
+++ NFE.js
@@ -5,3 +5,12 @@ function fact(n) {
 }
 
 console.log(fact(4));
+
+var myFun = function helper(n) {
+  if(n < 0) return;
+  if(n === 0 || n === 1) return 1;
+
+  return n * fact(n - 1);
+}
+
+console.log(myFun(5)); 
