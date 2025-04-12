a7bd503ec3d86e81fb8de795de775137cd1934a8
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-12 14:14:29 +0530

    closure example

--- closures.js
+++ closures.js
@@ -78,3 +78,27 @@ referenceObject.value++;
 
 foo(4);
 bar(4); 
+
+//example
+function mysteriousCalculator(a, b) {
+  var mysteriousVariable = 3;
+  return {
+      add: function() {
+          var result = a + b + mysteriousVariable;
+          return toFixedTwoPlaces(result);
+      },
+
+      subtract: function() {
+          var result = a - b - mysteriousVariable;
+          return toFixedTwoPlaces(result);
+      }
+  }
+}
+
+function toFixedTwoPlaces(value) {
+  return value.toFixed(2);
+}
+
+var myCalculator = mysteriousCalculator(10.01, 2.01);
+myCalculator.add() 
+myCalculator.subtract() 
