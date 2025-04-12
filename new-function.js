ef7ac96ba0c393c0c7242f4614783543ca03bb14
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-12 14:36:07 +0530

    example of new function scope is global not lexical scope

--- new-function.js
+++ new-function.js
@@ -2,3 +2,13 @@ let str = "hellouser";
 let func = new Function(str);
 
 func();
+
+function getFunc() {
+  let value = "test";
+
+  let func = new Function('alert(value)');
+
+  return func;
+}
+
+getFunc()(); //error
