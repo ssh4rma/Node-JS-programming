daf0d6ab63b67f19617ef58269840627d864524f
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-15 15:49:42 +0530

    duck typing polymorphism example

--- OOP.js
+++ OOP.js
@@ -4,3 +4,20 @@ console.log(n);
 console.log(typeof(n)); 
 //5.57
 //string
+
+//polymorphism
+
+class Duck {
+  type = () => console.log('Hello'.repeat(3));
+}
+
+class Cat {
+  type = () => console.log('World'.repeat(3));
+}
+
+let makeDucksType = (possibleDuckImposter) => possibleDuckImposter.type(); 
+
+let duck = new Duck();
+let cat = new Cat();
+
+[duck, cat].forEach(obj => makeDucksType(obj));
