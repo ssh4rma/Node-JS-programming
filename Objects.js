8b30c246ac35650c56db2e1d37a9260d058f2814
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-16 14:38:45 +0530

    user service function public method for adding new user to the data

--- Objects.js
+++ Objects.js
@@ -2,3 +2,19 @@
 console.log(new Number(3) === new Number(3)); //false
 
 console.log(3 == new Number(3)); //true 
+
+
+//reusable data service that manages a list of users (objects) and provides methods to add, remove, and retrieve users.
+
+function userSerive() {
+  let users = [
+    {id: 1, name: "Bob", email: "bob@gmail.com"},
+    {id: 2, name: "Alice", email: "alice@gmail.com"},
+  ];
+
+  //public methods
+  this.addUser = function(newUser) {
+    newUser.id = users.length + 1; 
+    users.push(newUser);
+  }
+}
