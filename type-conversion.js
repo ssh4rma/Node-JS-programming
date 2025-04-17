1e2fbd49c42aacf2aca5b176cf4f6d3421ae630b
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-17 11:28:59 +0530

    type conversion example

--- type-conversion.js
+++ type-conversion.js
@@ -9,5 +9,9 @@ console.log(undefined - 5); //NaN
 console.log("3" * "2") //6
 
 console.log([] == ![]); //true
-// [] is truthy value, toString([]) -> "" ;toValue("") -> 0
+// [] is truthy value, toString([]) -> "" ;Number("") -> 0
 // !(true) = false ; 0 == false; and Number(false) = 0, 0 == 0 => true;
+
+console.log(0 && 'hello' || 5); //5
+//&& will return the first falsy value or last of all truthy value
+//|| will return the first truthy value, or last of all falsy value
