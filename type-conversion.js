396b6ea38aa8f131b42472d1c5b8c5fdac7be3d4
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-17 11:26:17 +0530

    type conversion example

--- type-conversion.js
+++ type-conversion.js
@@ -1,4 +1,4 @@
-console.log(null == undefined);
+console.log(null == undefined); //true
 
 console.log([] >= 0);
 
@@ -7,3 +7,7 @@ console.log(null + "10"); //null10
 console.log(undefined - 5); //NaN
 
 console.log("3" * "2") //6
+
+console.log([] == ![]); //true
+// [] is truthy value, toString([]) -> "" ;toValue("") -> 0
+// !(true) = false ; 0 == false; and Number(false) = 0, 0 == 0 => true;
