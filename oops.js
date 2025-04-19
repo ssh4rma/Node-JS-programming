ef14a923219e628b36199e9c56fe0cd3141d2595
Shubham <shubham.sharma4390@gmail.com>
2025-04-19 15:28:22 +0530

    inheritance example

--- oops.js
+++ oops.js
@@ -30,3 +30,37 @@ const myAccount = new BankAccount(1000);
 myAccount.deposit(500);
 myAccount.withdraw(200);
 myAccount.checkBalance();
+
+
+
+//Inheritance
+class Vehical {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  start() {
+    console.log('Starting your vehical'); 
+  }
+
+  getDetails() {
+    return `Car is made by ${this.make}, Model of the car is ${this.model} and the Year of Manufacturing is ${this.year}`;
+  }
+}
+
+class Car extends Vehical {
+  constructor(make, model, year, seats) {
+    super(make, model, year);
+    this.seats = seats;
+  }
+
+  getDetails() {
+    return `${super.getDetails()} with ${this.seats} seats`;
+  }
+}
+ 
+
+const myCar = new Car("Hyundai", "Venue", 2021, 5);
+myCar.start(); 
+console.log(myCar.getDetails()); 
