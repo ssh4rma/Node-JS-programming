2683720a8e8dc3c31c223de54752f7b27015b06b
Shubham <shubham.sharma4390@gmail.com>
2025-04-19 11:01:35 +0530

    flatten the array using recursion

--- flatmap-and-flat.js
+++ flatmap-and-flat.js
@@ -2,7 +2,7 @@ let arr = [1,[2,5,[2,[5,1]],5]];
 
 arr.flat(1); //returns a new array.
 
-//
+//flatMap() Implementation
 let res = arr.flatMap((val) => {
   if (Array.isArray(val)) {
     return val.flat(1); 
@@ -10,3 +10,27 @@ let res = arr.flatMap((val) => {
     return val * 2;
   }
 });
+
+//recursively flatten the array
+function helper(arr) {
+  let flag = true;
+  for(const ele of arr) {
+    if(ele.length > 1) {
+      flag = false;
+      break;
+    }
+  } 
+  if(flag) return arr;
+
+  let res = [];
+  for(const ele of arr) {
+    if(Array.isArray(ele)) {
+      res.push(...helper(ele));
+    } else {
+      res.push(ele);
+    }
+  }
+  return res; 
+}
+
+console.log(helper(arr));
